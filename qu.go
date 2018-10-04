2f52d753aa0531ed61e085da35697d73b4c4971b
Charly <charlyfb90@gmail.com>
2018-10-05 01:56:04 +0200

    Update qu.go

--- qu.go
+++ qu.go
@@ -7,7 +7,7 @@ type QuHandler struct {
 	Next Handler
 }
 
-// Translate is the method to translate the words beginning with qu
+// Translate is the method to translate the words begining with qu
 func (q *QuHandler) Translate(w Word) Word {
 	if ok, err := startQU(w.GetOriginal()); !ok && err == nil {
 		log.Printf("QuHandler:[%s] not for me", w.GetOriginal())
